1ef4bf9cd2a69af31faab24b1b6a9f6ed914b130
Nagender kumar <kumarnagender811@gmail.com>
2024-12-23 18:51:27 +0530

    add Features component and integrate it into the App layout; enhance animation utilities

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,24 +1,48 @@
-export const animateWithGsapTimeline = (timeline, rotationRef, rotaionState, firstTarget, secondTarget, animationProps) => {
-    timeline.to(rotationRef.current.rotation, {
-        y: rotaionState,
-        duration:1,
-        ease: 'power2.inOut'
-    })
+import gsap from "gsap";
 
-    timeline.to(
-        firstTarget,
-        {
-            ...animationProps,
-            ease: 'power2.inOut'
-        },
-        '<'
-    )
-    timeline.to(
-        secondTarget,
-        {
-            ...animationProps,
-            ease: 'power2.inOut'
-        },
-        '<'
-    )
-}
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+export const animationWithGsap = (target, animationProps, scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
+};
+
+export const animateWithGsapTimeline = (
+  timeline,
+  rotationRef,
+  rotaionState,
+  firstTarget,
+  secondTarget,
+  animationProps
+) => {
+  timeline.to(rotationRef.current.rotation, {
+    y: rotaionState,
+    duration: 1,
+    ease: "power2.inOut",
+  });
+
+  timeline.to(
+    firstTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+  timeline.to(
+    secondTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+};
