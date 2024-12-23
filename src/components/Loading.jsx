b8d98aba4144dde1e7da38a8ab87a284cde50f06
Nagender kumar <kumarnagender811@gmail.com>
2024-12-23 15:53:03 +0530

    refactor Loading component to enhance loading animation and improve code formatting

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,16 +1,18 @@
-import { Html } from '@react-three/drei'
-import React from 'react'
+import { Html } from '@react-three/drei';
+import React from 'react';
 
 const Loading = () => {
   return (
     <Html>
-      <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
-        <div className='w-[10vw] h-[10vh] rounded-full'>
-          Loading...
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+        <div className="flex items-center justify-center space-x-2">
+          <div className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"></div>
+          <div className="w-4 h-4 bg-green-500 rounded-full animate-bounce delay-150"></div>
+          <div className="w-4 h-4 bg-red-500 rounded-full animate-bounce delay-300"></div>
         </div>
       </div>
     </Html>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
